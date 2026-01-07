6debb40fd97b80c3a31f9add490c603b9dc20cab
erankaner2011-coder <erankaner2011@gmail.com>
2026-01-07 08:39:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Download the data from your GitHub repository
+!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
+!wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
+import lab_setup_do_not_edit
+
 import pandas
 df = pandas.read_csv("/content/parkinsons.csv")
 selected_features = ['PPE', 'MDVP:Fo(Hz)']
